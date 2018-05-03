1a7e2efd7ec6690b5dd9ce071613a7ccd25df1c7
Sydbob <kiva1102@Gmail.com>
2018-05-03 11:40:31 +0100

    registration sorta fixed

--- src/main/java/com/doIt/DoIt/entity/MemberDTO.java
+++ src/main/java/com/doIt/DoIt/entity/MemberDTO.java
@@ -5,59 +5,30 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.annotation.Transient;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.util.Set;
-/**
- * Class that corresponds to Member entity in the database
- * Data members are named accordingly to the entity attributes in the database
- * Certain fields are annotated used @ annotations
- */
-@Entity
-public class Member implements Serializable {
 
-    @Id
-    @NotEmpty(message = "Username cannot be empty")
-    @Column(name ="username")
+/**
+ * Member Data Transfer Object class that will be used to pass newly registered  member's data to the back-end
+ * */
+public class MemberDTO {
+
+
     private String username;
 
-    @NotEmpty(message = "Please provide a password")
-    @Length(min = 5, max = 30, message = "Your password must be 5 and 30 characters long")
-    @Transient
-    @Column(name = "password")
+
     private String password;
 
-    @Column(name = "teamID")
+
     private int teamID;
 
-    @Column(name = "name")
-    @NotEmpty(message = "Please provide a name")
     private String name;
 
-    @Column(name = "email")
-    @NotEmpty(message = "Please provide an email")
     private String email;
-
-    @Column(name = "telephone")
-    @NotEmpty(message = "Please provide a phone number")
     private String telephone;
 
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "member_roles", joinColumns = @JoinColumn(name = "username"), inverseJoinColumns = @JoinColumn(name = "roleID"))
     private Set<Role> roles;
-
-    //===============constructor(s)=================
-    public Member() {}
-    public Member(String username, String password, int teamID, String name, String email, String telephone, Set<Role> roles) {
-        this.username = username;
-        this.password = password;
-        this.teamID = teamID;
-        this.name = name;
-        this.email = email;
-        this.telephone = telephone;
-        this.roles = roles;
-    }
-
-    //===============getters and setters=================
 
     public String getUsername() {
         return username;
